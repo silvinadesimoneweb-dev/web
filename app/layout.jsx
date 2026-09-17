efdfe8fd5a52bb8e488a51afc3b3faa56d38be1f
silvinadesimoneweb-dev <silvinadesimoneweb@gmail.com>
2026-09-17 01:00:36 -0300

    Eliminar layout anterior

--- app/layout.jsx
+++
@@ -1,9 +0,0 @@
-export const metadata = {
-  title: "Psicóloga Online | Lic. Silvina De Simone",
-  description: "Atención psicológica online para jóvenes, adultos, adolescentes, parejas y argentinos en el exterior.",
-  icons: { icon: "/favicon.svg" }
-};
-
-export default function RootLayout({ children }) {
-  return <html lang="es"><body>{children}</body></html>;
-}
